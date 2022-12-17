7af7d53e316efff138f58b48a595d9e6fc628bc3
Konstantino123 <kt79@bk.ru>
2022-12-17 19:23:18 +0300

    My pro

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿// Написать программу которая из имеющегося массива строк, сформирует массив строк длина которых меньше 3.
+            string[] Massiv;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            Massiv = stroka.Split(' ');
+            var result = new string[Massiv .Length];
+            var realSize = 0;
+            foreach (var value in Massiv )
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
